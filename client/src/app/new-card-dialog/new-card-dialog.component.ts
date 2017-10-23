daad9da5e6ee4d07b1543a03bd9f12a4c31683f4
unwarysage <shawx538@morris.umn.edu>
2017-10-23 10:51:21 -0500

    fix Type issues in underlying code.
    including MatDialogConfig, and MatSnackbarConfig
    
    @floogulinc

--- client/src/app/new-card-dialog/new-card-dialog.component.ts
+++ client/src/app/new-card-dialog/new-card-dialog.component.ts
@@ -1,6 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {DeckService} from "../deck/deck.service";
-import {MatDialogRef, MAT_DIALOG_DATA, MatSnackBar} from "@angular/material";
+import {MatDialogRef, MAT_DIALOG_DATA, MatSnackBar, MatSnackBarConfig } from "@angular/material";
 
 @Component({
   selector: 'app-new-card-dialog',
@@ -34,16 +34,16 @@ export class NewCardDialogComponent implements OnInit {
             succeeded => {
                 //this.cardAddSuccess = true;
                 this.matDialogRef.close(succeeded);
-                this.snackBar.open("Added card", null, {
-                    duration: 2000,
-                });
+                let config = new MatSnackBarConfig();
+                config.duration = 2000;
+                this.snackBar.open("Added card", null, config);
                 //this.refreshDeck();
             },
             err => {
                 console.log(err);
-                this.snackBar.open("Error adding card", null, {
-                    duration: 2000,
-                });
+                let config = new MatSnackBarConfig();
+                config.duration = 2000;
+                this.snackBar.open("Error adding card", null, config);
             });
     }
 
