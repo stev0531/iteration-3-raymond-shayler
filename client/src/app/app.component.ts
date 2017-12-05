1c8b66f92fac5cd61d288abca4169be7e9f89e79
unwarysage <shawx538@morris.umn.edu>
2017-12-05 15:09:10 -0600

    add base implementation of checkAuthorization connectivity

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,9 +4,24 @@ import {AuthService} from "./auth/auth.service";
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
 })
 
 export class AppComponent {
     title = 'SAGE';
+    showTeachOptions= false;
+
+    constructor(private _authService: AuthService){}
+
+    ngOnInit(){
+        this._authService.authorized().subscribe(
+            newauth => {
+                this.showTeachOptions = newauth;
+            },
+            err => {
+                console.log(err);
+            }
+        );
+
+    }
 }
--- client/src/app/auth/auth.service.ts
+++ client/src/app/auth/auth.service.ts
@@ -1,19 +1,34 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Observable } from "rxjs";
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {Observable} from "rxjs/Observable";
+import {environment} from "../../environments/environment.prod";
 
 @Injectable()
 export class AuthService {
-    constructor(private http:Http) { }
+    constructor(private http: Http) {
+    }
 
-    authorized() : Observable<boolean> {
+    authorized(): Observable<boolean> {
         console.log("checking if I am logged in");
-        return this.http.get("/api/" + "checkAuthorization", {withCredentials: true}).map(res => res.json().authorized);
+
+        // let result = this.http.get("/api/" + "checkAuthorization").flatMap(
+        //     (response: Response) => {
+        //         // return Observable.of({type: "success", authorized: response.json()})
+        //         return Observable.map(res => res.json())
+        //     });
+        //
+        // console.log(result);
+        // return result.authorized
+
+        let result: Observable<any> = this.http.request(environment.API_URL + "/checkAuthorization", {withCredentials:true});
+        return result.map(res => res.toString().authorized);
     }
 
 
-    login(){
+    login() {
         console.log("Start Login")
         this.http.request("/api/authorise")
     }
+
+    onIni
 }
