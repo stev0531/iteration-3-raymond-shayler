4ae5ca48b0b31187bc62e8f73550deea23f760ca
stev0531 <stev0531@morris.umn.edu>
2017-12-06 22:44:03 -0600

    Fix styling on results, select-color, and start-screen
    The scores on the results pop-up now display
    in a vertical line. For select-color, it's a
    little more obvious that the player number changes,
    and the help button on the start-screen is now
    in line with the title on a small phone screen.

--- client/src/app/select-color/select-color.component.ts
+++ client/src/app/select-color/select-color.component.ts
@@ -8,17 +8,14 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class SelectColorComponent implements OnInit {
 
-  public colors = ['red', 'org', 'ylw', 'grn', 'blu', 'pur'];
   public color: string;
   public deckPlus: string;
-  public playerColors = [];
   public numOfPlayers: number;
   public player = 1;
 
   constructor(private route: ActivatedRoute) { }
 
     select(color) {
-        this.playerColors.push(color);
         this.deckPlus = this.deckPlus + '_' + color;
         this.player++;
     }
@@ -29,7 +26,6 @@ export class SelectColorComponent implements OnInit {
           if(this.deckPlus != null){
               let splitStr = this.deckPlus.split("_", 3);
               this.numOfPlayers = Math.abs(+splitStr[2]);
-
           }
 
       });
